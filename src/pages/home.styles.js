b1c8daef6d75b12bf3a123c1ddf20c7d11eee80e
Habibur Rahman <hrsshainik@gmail.com>
2021-02-02 10:16:52 +0600

    Extracting navbar form home.js

--- src/pages/home.styles.js
+++ src/pages/home.styles.js
@@ -15,49 +15,6 @@ export const HomeContainer = styled.div`
   }
 `;
 
-export const Navbar = styled.nav`
-  width: 100%;
-  height: 8rem;
-  background-color: var(--bgOne);
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  position: fixed;
-  bottom: 0;
-  z-index: 1000;
-`;
-
-export const NavLink = styled.a`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  transition: color 300ms ease;
-
-  :hover {
-    color: var(--primary);
-    cursor: pointer;
-  }
-`;
-
-export const NavLogo = styled.svg`
-  width: 3.2rem;
-  display: block;
-  margin-bottom: 0.8rem;
-  fill: var(--white);
-  transition: fill 300ms ease;
-
-  ${NavLink}:hover & {
-    fill: var(--primary);
-  }
-`;
-
-export const NavText = styled.p`
-  text-transform: uppercase;
-  font-weight: 600;
-  font-size: 1.2rem;
-`;
-
 export const LogoContainer = styled.a`
   cursor: pointer;
 `;
