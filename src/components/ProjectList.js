d184743232f99bc5cdd9dbd41f5972d97dca0d68
Habibur Rahman <hrsshainik@gmail.com>
2022-10-01 23:01:24 +0600

    project serial problem fixed

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -7,7 +7,7 @@ const ProjectList = ({ projects }) => {
   return (
     <>
       {projects.map(project => {
-        if (project.frontmatter.id % 2 === 0) {
+        if (project.frontmatter.id === 1) {
           return (
             <div className={styles.projectThumb} key={project.id}>
               <div className={styles.colL}></div>
@@ -21,7 +21,7 @@ const ProjectList = ({ projects }) => {
               </Link>
             </div>
           )
-        } else if (project.frontmatter.id % 2 === 1) {
+        } else if (project.frontmatter.id === 2) {
           return (
             <div className={styles.projectThumb} key={project.id}>
               <Link
@@ -33,6 +33,48 @@ const ProjectList = ({ projects }) => {
               </Link>
 
               <div className={styles.colR}></div>
+            </div>
+          )
+        } else if (project.frontmatter.id === 3) {
+          return (
+            <div className={styles.projectThumb} key={project.id}>
+              <div className={styles.colL}></div>
+
+              <Link
+                className={`${styles.colR} ${styles.project}`}
+                to={`/project/${project.frontmatter.slug}`}
+              >
+                <h5>{project.frontmatter.title}</h5>
+                <Img fluid={project.frontmatter.thumb.childImageSharp.fluid} />
+              </Link>
+            </div>
+          )
+        } else if (project.frontmatter.id === 4) {
+          return (
+            <div className={styles.projectThumb} key={project.id}>
+              <Link
+                className={`${styles.colL} ${styles.project}`}
+                to={`/project/${project.frontmatter.slug}`}
+              >
+                <h5>{project.frontmatter.title}</h5>
+                <Img fluid={project.frontmatter.thumb.childImageSharp.fluid} />
+              </Link>
+
+              <div className={styles.colR}></div>
+            </div>
+          )
+        } else if (project.frontmatter.id === 5) {
+          return (
+            <div className={styles.projectThumb} key={project.id}>
+              <div className={styles.colL}></div>
+
+              <Link
+                className={`${styles.colR} ${styles.project}`}
+                to={`/project/${project.frontmatter.slug}`}
+              >
+                <h5>{project.frontmatter.title}</h5>
+                <Img fluid={project.frontmatter.thumb.childImageSharp.fluid} />
+              </Link>
             </div>
           )
         }
