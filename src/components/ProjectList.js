47080b4504a6b6b2190deced55bf8df95470ba3d
Habibur Rahman <hrsshainik@gmail.com>
2022-10-03 02:33:37 +0600

    card reveal animation done

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,25 +1,14 @@
-import React, { useEffect } from "react"
+import React from "react"
 import Img from "gatsby-image"
 import { Link } from "gatsby"
 import * as styles from "../styles/projectList.module.css"
 import gsap from "gsap"
-import CSSRulePlugin from "gsap/CSSRulePlugin"
 import ScrollTrigger from "gsap/ScrollTrigger"
 
 gsap.registerPlugin(ScrollTrigger)
-let tl = gsap.timeline()
 
 const ProjectList = ({ projects }) => {
-  let imgReveal = CSSRulePlugin.getRule(".cardImgCon:after")
-
-  useEffect(() => {
-    tl.to(imgReveal, 1.4, { width: "0%" }).to(".cardImg", {
-      duration: 1.4,
-      scale: 1.6,
-      delay: -1.6,
-    })
-  }, [imgReveal])
-
+  console.log(projects)
   return (
     <>
       {projects.map(project => (
@@ -33,8 +22,12 @@ const ProjectList = ({ projects }) => {
             className={`${styles.col} ${styles.project}`}
             to={`/project/${project.frontmatter.slug}`}
           >
-            <h5>{project.frontmatter.title}</h5>
-            <div className="cardImgCon">
+            <h5 className="cardTitle">{project.frontmatter.title}</h5>
+            <div
+              className={`cardImgCon cardImgCon${
+                project.frontmatter.id % 2 === 0 ? "Even" : "Odd"
+              }`}
+            >
               <Img
                 className="cardImg"
                 fluid={project.frontmatter.thumb.childImageSharp.fluid}
